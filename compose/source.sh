9dfb1f257643165b3444561c22fd312b375adc49
xaduha <xaduha@gmail.com>
2016-06-18 02:11:29 +0300

    MISC commands to test datastore

---
+++ compose/source.sh
@@ -0,0 +1,12 @@
+unset RDS
+export MY_API_KEY=16abcd26-793e-475a-ad8f-fed400ef5d7a
+export MY_RESOURCE_ID=bf133581-8672-473a-9789-02953f85051e
+export MY_PACKAGE_ID=test
+export CKAN_DB_NAME=ckan_default
+export CKAN_DB_USER=ckan_default
+export CKAN_DATASTORE_DB_NAME=datastore_default
+export CKAN_DATASTORE_RW_DB_USER=ckan_default
+export CKAN_DATASTORE_RO_DB_USER=datastore_default
+export CKAN_DB_USER_PASS=pass
+export CKAN_DATASTORE_RW_DB_USER_PASS=pass
+export CKAN_DATASTORE_RO_DB_USER_PASS=pass
