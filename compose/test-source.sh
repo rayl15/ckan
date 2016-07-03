dd2f7fab72ff3a2fa72c9d847a246c88cb7e6d0e
xaduha <xaduha@gmail.com>
2016-07-03 05:35:02 +0300

    #5396 harvesting appears to work, reenabling other harvesters

--- compose/test-source.sh
+++ compose/test-source.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-make paster harvester source barnet open.barnet.gov.uk ckan
+#make paster harvester source barnet open.barnet.gov.uk ckan
+docker exec `docker ps | grep spatial | cut -d ' ' -f1` sh -c "cd /srv/app/src/ckan && paster --plugin=ckanext-harvest harvester source barnet https://open.barnet.gov.uk ckan -c /srv/app/production.ini"
 make paster harvester run_test barnet
+:
+:
+:
+:
