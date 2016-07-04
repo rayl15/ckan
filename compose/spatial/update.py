bc1e70882026b78ae4c8b4180561cb5b18ac4a25
xaduha <xaduha@gmail.com>
2016-07-04 06:14:37 +0300

    #5396 removed unused spatial folder

--- compose/spatial/update.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python
-import pip
-from subprocess import call
-
-for dist in pip.get_installed_distributions():
-    if 'ckanext' in dist.project_name:
-        call("pip install --upgrade " + dist.project_name, shell=True)
